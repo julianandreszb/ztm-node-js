683c8e80e55dd52e244ae7174ece79fc34a4c300
jzapata <jzapata@lifefile.net>
2023-08-03 23:16:25 -0500

    http server + params

--- 5-http-server.js
+++ 5-http-server.js
@@ -1,17 +1,38 @@
 const http = require('http');
 
 const PORT = 3000;
-const server = http.createServer((req, res) => {
-    if (req.url === '/friends') {
+
+const server = http.createServer();
+
+const friends = [
+    {
+        id: 1,
+        name: "Julian Zapata"
+    },
+    {
+        id: 2,
+        name: "Hellen"
+    },
+    {
+        id: 3,
+        name: "ian"
+    },
+];
+
+server.on('request', (req, res) => {
+    const items = req.url.split('/');
+    if (items[1] === 'friends') {
         // res.writeHead(200, {
         //     'Content-Type': 'application/json',
         // });
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({
-            name: 'Julian Zapata'
-        }));
-    } else if (req.url === '/messages') {
+        res.setHeader('Content-Type', 'application/json');
+        if(items.length === 3){
+            res.end(JSON.stringify(friends[items[2]]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+    } else if (items[1] === 'messages') {
         // res.writeHead(200, {
         //     'Content-Type': '',
         // });
